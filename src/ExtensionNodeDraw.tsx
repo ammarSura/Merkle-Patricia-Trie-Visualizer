17c2a4e7ff7845d4a042d6c250133cde5e7124e9
ammarSura <ammarsura@gmail.com>
2023-12-01 18:35:43 +0530

    feat: render sub branches seperately

--- src/ExtensionNodeDraw.tsx
+++ src/ExtensionNodeDraw.tsx
@@ -1,7 +1,10 @@
 import { ExtensionNode } from "@ethereumjs/trie"
 import NodeButton, { CoorsType } from "./NodeButton"
 import { MPT } from "./mpt"
-import { handleNodeRender } from "./handleNodeRender"
+import { Link } from "react-router-dom"
+import { bytesToHex } from "@ethereumjs/util"
+import { ArrowDownIcon } from "@chakra-ui/icons"
+import { IconButton } from "@chakra-ui/react"
 import nodeStyle from "./node-style"
 import LineDraw from "./LineDraw"
 
@@ -9,22 +12,23 @@ type ExtensionNodeProps = {
     trieNode: ExtensionNode
     coors: CoorsType
     mpt: MPT
+    drawChildren?: boolean
 }
 
-export default function ExtensionNodeDraw(props: ExtensionNodeProps) {
-    const { trieNode, coors, mpt } = props
+export default function ExtensionNodeDraw({
+    trieNode,
+    coors,
+}: ExtensionNodeProps) {
     const key = trieNode.key()
     const value = trieNode.value()
-    const decodedChild = mpt.getDecodedNodeFromDb(value as Uint8Array)
     const nextNodeHeightFactor = 1.75
     const {
         x,
         y,
-        childOffset
     } = coors
     const nextCoors = {
         x: x,
-        y: y + ((nodeStyle.height as number) * nextNodeHeightFactor) + (childOffset || 0)
+        y: y + ((nodeStyle.height as number) * nextNodeHeightFactor)
     }
     return (
         <>
@@ -34,8 +38,9 @@ export default function ExtensionNodeDraw(props: ExtensionNodeProps) {
                 value={value}
                 label={"Extn"}
                 backgroundColor={"#74a1db"}
+                showEmptyKeyLabel={true}
             />
-            <LineDraw
+                        <LineDraw
                 startPoint={{
                     x: coors.x,
                     y: coors.y
@@ -45,9 +50,25 @@ export default function ExtensionNodeDraw(props: ExtensionNodeProps) {
                     y: nextCoors.y
                 }}
             />
-            {
-                handleNodeRender(decodedChild, nextCoors, mpt)
-            }
-       </>
+
+            <Link
+                to={{
+                    pathname: '/',
+                    search: `?nodeDbKey=${bytesToHex(value)}`
+                }}>
+                <IconButton
+                    style={{
+                       ...nodeStyle,
+                        left: coors.x,
+                        top: coors.y + (nodeStyle.height as number) * 1.7,
+                    }}
+                    bg='#b8b7b4'
+                    aria-label="Go to next node"
+                    icon={<ArrowDownIcon boxSize={'1.5rem'}/>}
+                >
+                </IconButton>
+
+            </Link>
+        </>
     )
 }
